f7b25f4dd305d9aca5796ea86ac8a4e585cef051
Lontra Felina <26169724+Tschebbischeff@users.noreply.github.com>
2026-05-01 01:53:52 +0200

    uses module name as prefix to extension instead of prefix to whole file; uses dirname as module name

--- habitat-config/update-config.sh
+++ habitat-config/update-config.sh
@@ -3,15 +3,23 @@
 cd "/habitat-config" || exit 1
 
 for dirPath in "./source/"*; do
-    echo "*** Applying configuration for module '$APP_MODULE_NAME' (Source directory: /habitat-config/source/$(basename "$dirPath"))..."
+    moduleName="$(basename "$dirPath")"
+    echo "*** Applying configuration for module '$moduleName' (Source directory: /habitat-config/source/$moduleName)..."
     # Traefik Dynamic Config
     if [ -d "./target/traefik" ]; then
         echo "Traefik config:"
-        rm -f "./target/traefik/${APP_MODULE_NAME}."*
+        for f in "./target/traefik/"*; do
+            filename=$(basename -- "$f")
+            filename="${filename%.*}"
+            [[ "$filename" == *".$moduleName" ]] && rm -f "$f"
+        done
         if [ -d "$dirPath/traefik" ]; then
             for f in "$dirPath/traefik/"*; do
-                echo " $f -> ./target/traefik/${APP_MODULE_NAME}.$(basename "$f")"
-                cp "$f" "./target/traefik/${APP_MODULE_NAME}.$(basename "$f")";
+                filename=$(basename -- "$f")
+                extension="${filename##*.}"
+                filename="${filename%.*}"
+                echo " $f -> ./target/traefik/$filename.$moduleName.$extension"
+                cp -f "$f" "./target/traefik/$filename.$moduleName.$extension";
             done
         else
             echo " None"
