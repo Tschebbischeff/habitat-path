5121ab3644ed9d377358acdad2215e3a1976f1aa
Lontra Felina <26169724+Tschebbischeff@users.noreply.github.com>
2026-04-30 22:40:19 +0200

    sets network and enables env vars for habitat-config container; uses subdirectory for each module inside config source volume

--- habitat-config/update-config.sh
+++ habitat-config/update-config.sh
@@ -2,10 +2,14 @@
 
 cd "/habitat-config" || exit 1
 
-# Traefik Dynamic Config
-if [ -d "./source/traefik" ]; then
-    rm -f "./target/traefik/${APP_MODULE_NAME}."*
-    for f in "./source/traefik/"*; do
-        cp "$f" "./target/traefik/${APP_MODULE_NAME}.$(basename "$f")";
-    done
-fi
+for dirPath in "./source/"*; do
+    echo "Applying configuration for '$APP_MODULE_NAME' ($(basename "$dirPath"))"
+    # Traefik Dynamic Config
+    echo "Applying traefik config"
+    if [ -d "./source/$dirPath/traefik" ]; then
+        rm -f "./target/traefik/${APP_MODULE_NAME}."*
+        for f in "./source/$dirPath/traefik/"*; do
+            cp "$f" "./target/traefik/${APP_MODULE_NAME}.$(basename "$f")";
+        done
+    fi
+done
