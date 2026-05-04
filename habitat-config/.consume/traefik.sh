c0ed95ebb2e563459c3c3ab93cfeb8ce47a652be
Lontra Felina <26169724+Tschebbischeff@users.noreply.github.com>
2026-05-04 14:06:53 +0200

    uses pre-merged and overridden config, simplifying consumer logic on module side significantly

--- habitat-config/.consume/traefik.sh
+++ habitat-config/.consume/traefik.sh
@@ -2,72 +2,11 @@
 
 set -euo pipefail
 
-SOURCE_PATH="$1"
+SOURCE_PATH="$1"; [ -d "$SOURCE_PATH" ] || { echo "'$SOURCE_PATH' not found."; exit 1; }
 TARGET_PATH="/habitat-config/target/traefik"
-[ -z "$SOURCE_PATH" ] && { echo "No configuration source directory provided."; exit 1; }
-[ -d "$SOURCE_PATH" ] || { echo "Could not find configuration source directory '$SOURCE_PATH'."; exit 1; }
-[ -z "$TARGET_PATH" ] && { echo "No configuration target directory provided."; exit 1; }
-[ -d "$TARGET_PATH" ] || { echo "Could not find configuration target directory '$TARGET_PATH'."; exit 1; }
-MERGE_PATH="$SOURCE_PATH/.merged"
-mkdir -p "$MERGE_PATH"
 
-mergeYAML() {
-    local sourceFile="$1"
-    local targetFile="$2"
-    local mergedFileContents="";
-    echo "Merging '${sourceFile#"$SOURCE_PATH/"}' into '${targetFile#"$SOURCE_PATH/"}'"
-    mkdir -p "$(dirname -- "$targetFile")"
-    touch "$targetFile"
-    # shellcheck disable=SC2016 # $item variable is part of yq expression and not to be expanded
-    mergedFileContents="$(yq eval-all '. as $item ireduce ({}; . * $item )' "$targetFile" "$sourceFile")"
-    echo "$mergedFileContents" >"$targetFile"
-}
-
-for moduleSrcPath in "$SOURCE_PATH/"*; do
-    [ ! -d "$moduleSrcPath" ] && continue
-    echo "Merging configuration provided by '$(basename "$moduleSrcPath")' module..."
-    (
-        cd "$moduleSrcPath"
-        find . -name '*.yml' -type f -printf '%P\n' | while read -r cfgRelFilePath; do
-            mergeYAML "$moduleSrcPath/$cfgRelFilePath" "$MERGE_PATH/$cfgRelFilePath"
-        done
-    )
-done
-
-echo "Merging based on priorities..."
-(
-    cd "$MERGE_PATH"
-    find . -name '*.yml' -type f -printf '%P\n' | while read -r cfgRelFilePath; do
-        cfgFileName="$(basename -- "$cfgRelFilePath")"
-        cfgFileExtension="${cfgFileName##*.}"
-        cfgFileBaseName="${cfgFileName%.*}"
-        cfgFilePriority="${cfgFileBaseName##*.}"
-        re='^hbtprio-(override-)?[0-9]+$'
-        if ! [[ "$cfgFilePriority" =~ $re ]] || [ "$cfgFilePriority" == "$cfgFileBaseName" ]; then
-            continue
-        fi
-        cfgFileBaseName="${cfgFileBaseName%.*}"
-        cfgFilePriority="${cfgFilePriority##hbtprio-}"
-        overrideConfigFile=""
-        if echo "$cfgFilePriority" | grep -q '^override-'; then
-            overrideConfigFile="_"
-            cfgFilePriority="${cfgFilePriority##override-}"
-        fi
-        cfgFilePriority="$(( cfgFilePriority + 0 ))"
-        sourceFile="$MERGE_PATH/$cfgRelFilePath"
-        targetFile="$MERGE_PATH/$(dirname -- "$cfgRelFilePath")/$cfgFileBaseName.$cfgFileExtension"
-        if [ -n "$overrideConfigFile" ]; then
-            echo "Overriding '${sourceFile#"$SOURCE_PATH/"}' over '${targetFile#"$SOURCE_PATH/"}'"
-            cp -f "$sourceFile" "$targetFile"
-        else
-            mergeYAML "$sourceFile" "$targetFile"
-        fi
-        rm "$sourceFile"
-    done
-)
-
-echo "Copying final configuration to '$TARGET_PATH'"
+echo "Cleaning previous configuration at '$TARGET_PATH'"
 rm -rf "${TARGET_PATH:?}/"{*,.*} &>/dev/null
+echo "Copying new configuration to '$TARGET_PATH'"
 cp -rp "$MERGE_PATH/." "$TARGET_PATH/"
-
 exit 0
