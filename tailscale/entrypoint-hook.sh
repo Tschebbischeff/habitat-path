25efa9a1e193a03c017775549518fee1d7b3f1ac
Lontra Felina <26169724+Tschebbischeff@users.noreply.github.com>
2026-05-06 13:57:49 +0200

    makes tailscale configurable cross-module

--- tailscale/entrypoint-hook.sh
+++ tailscale/entrypoint-hook.sh
@@ -1,17 +1,11 @@
 #!/bin/sh
 
-while :; do
-  socat TCP-LISTEN:80,bind=127.0.0.1,reuseaddr,fork "TCP:${APP_NAME_HOST}_traefik:80"
-  sleep 1
-done &
-while :; do
-  socat TCP-LISTEN:443,bind=127.0.0.1,reuseaddr,fork "TCP:${APP_NAME_HOST}_traefik:443"
-  sleep 1
-done &
-while :; do
-  socat UDP-LISTEN:8189,bind=127.0.0.1,reuseaddr,fork "UDP:${APP_NAME_HOST}_traefik:8189"
-  sleep 1
-done &
+cat "$TS_SERVE_CONFIG" | grep -Po '127\.0\.0\.1:\K[0-9]*' | while read -r port; do
+  while :; do
+    socat "TCP-LISTEN:$port,bind=127.0.0.1,reuseaddr,fork" "TCP:${APP_NAME_HOST}_traefik:$port"
+    sleep 1
+  done &
+done
 
 [ -n "$TS_AUTHKEY_FILE" ] && export TS_AUTHKEY="$(cat "$TS_AUTHKEY_FILE")"
 
