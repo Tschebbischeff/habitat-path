4bc0e3e5ae8c09e16f6d3cf77c71f1f2f566244a
Lontra Felina <26169724+Tschebbischeff@users.noreply.github.com>
2026-09-07 15:12:54 +0200

    makes entrypoint hook an actual entrypoint and stops socat processes when the main process stops

--- tailscale/entrypoint-hook.sh
+++ tailscale/entrypoint-hook.sh
@@ -1,12 +1,14 @@
 #!/bin/sh
 
-cat "$TS_SERVE_CONFIG" | grep -Po '127\.0\.0\.1:\K[0-9]*' | while read -r port; do
-  while :; do
+MAIN_PID=$$
+
+cat "$TS_SERVE_CONFIG" | grep -Po '127\.0\.0\.1:\K[0-9]*' | while read -r port; do (
+  while kill -0 "$MAIN_PID" 2>/dev/null; do
     socat "TCP-LISTEN:$port,bind=127.0.0.1,reuseaddr,fork" "TCP:${APP_NAME_HOST}_traefik:$port"
     sleep 1
-  done &
-done
+  done
+) & done
 
 [ -n "$TS_AUTHKEY_FILE" ] && export TS_AUTHKEY="$(cat "$TS_AUTHKEY_FILE")"
 
-/usr/local/bin/containerboot
+exec "$@"
